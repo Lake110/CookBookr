a02e0ad9c5c6a590f3b84a0c450dedb159bc3406
Lake110 <mlake0911@gmail.com>
2025-09-29 14:48:43 +0100

    Implement RecipeAdmin class in admin.py with list display, filters, search fields, and organized fieldsets

--- Recipes/admin.py
+++ Recipes/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 
+        'author', 
+        'prep_time', 
+        'cook_time', 
+        'servings', 
+        'created_at'
+    ]
+    list_filter = ['created_at', 'author', 'prep_time', 'cook_time']
+    search_fields = ['title', 'ingredients', 'description']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    # Organize the admin form into sections
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'description', 'author')
+        }),
+        ('Recipe Details', {
+            'fields': ('ingredients', 'instructions', 'servings')
+        }),
+        ('Timing', {
+            'fields': ('prep_time', 'cook_time')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
