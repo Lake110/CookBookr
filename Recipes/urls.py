e80257c88b58d27ed332f5b2297665c290a00baf
Lake110 <mlake0911@gmail.com>
2025-10-01 13:15:38 +0100

    Add URL pattern for recipe detail view

--- Recipes/urls.py
+++ Recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.recipe_home, name='recipes_home'),
     path('add/', views.add_recipe, name='add_recipe'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
